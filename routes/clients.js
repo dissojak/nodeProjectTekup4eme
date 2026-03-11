bb8a67b9c7aec1dbe5d42c467fd3206e9d1b45e3
dissojak <dissojak@gmail.com>
2026-03-11 20:28:17 +0100

    Adem: add swagger documentation to clients routes

--- routes/clients.js
+++ routes/clients.js
@@ -10,6 +10,116 @@ const {
 const { protect, authorize } = require('../middleware/authMiddleware');
 const { createClientValidator, updateClientValidator } = require('../validators/clientValidator');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Clients
+ *   description: Client management
+ */
+
+/**
+ * @swagger
+ * /api/clients:
+ *   get:
+ *     summary: Get all clients
+ *     tags: [Clients]
+ *     responses:
+ *       200:
+ *         description: List of clients
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Client'
+ *   post:
+ *     summary: Create a new client
+ *     tags: [Clients]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               phone:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Client created
+ *       400:
+ *         description: Validation error
+ */
+
+/**
+ * @swagger
+ * /api/clients/{id}:
+ *   get:
+ *     summary: Get client by ID
+ *     tags: [Clients]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Client data
+ *       404:
+ *         description: Client not found
+ *   put:
+ *     summary: Update a client
+ *     tags: [Clients]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               phone:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Client updated
+ *       404:
+ *         description: Client not found
+ *   delete:
+ *     summary: Delete a client
+ *     tags: [Clients]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Client deleted
+ *       404:
+ *         description: Client not found
+ */
+
 router.use(protect); // All client routes require authentication
 
 router
